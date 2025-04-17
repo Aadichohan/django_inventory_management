d532b299e7a8a1489c3956a805e8466a4e605cdb
Abdul Ahad <it.dev32@dawateislami.net>
2025-04-17 20:30:12 +0500

    product and store

--- store_product/models.py
+++ store_product/models.py
@@ -1,37 +1,39 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.conf import settings
 from product.models import Product
 from store.models import Store
 
 class StoreProduct(models.Model):
-    id           = models.AutoField(primary_key=True)
-    product   = models.ForeignKey(
+    id            = models.AutoField(primary_key=True)
+    
+    product       = models.ForeignKey(
         Product, related_name='product_store_product',
         on_delete=models.CASCADE
     )
-    store     = models.ForeignKey(
+    store         = models.ForeignKey(
         Store, related_name='product_store_store',
         on_delete=models.CASCADE
     )
-    quantity     = models.IntegerField()
-    custom_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    description  = models.TextField(max_length=500, null=True, blank=True)
-    is_active    = models.BooleanField(default=True)
-    created_at   = models.DateTimeField(auto_now_add=True)
-    created_by   = models.ForeignKey(
+    quantity      = models.IntegerField()
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    sell_price     = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    # custom_price   = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    description    = models.TextField(max_length=500, null=True, blank=True)
+    is_active      = models.BooleanField(default=True)
+
+    created_at     = models.DateTimeField(auto_now_add=True)
+    created_by     = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, related_name='product_store_created'
     )
-    # created_by   = models.ForeignKey(
-    #     User, related_name='product_store_created',
-    #     on_delete=models.SET_NULL, null=False
-    # )
-    updated_at   = models.DateTimeField(null=True, blank=True)  # manually handled
-    updated_by   = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='product_store_updated', on_delete=models.SET_NULL, null=True, blank=True)
+    updated_at     = models.DateTimeField(null=True, blank=True)
+    updated_by     = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='product_store_updated', on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
         return f'{self.product.title} - {self.store.title}'
-    
 
     class Meta:
-        db_table = 'store_product' 
+        db_table = 'store_product'
+
+
